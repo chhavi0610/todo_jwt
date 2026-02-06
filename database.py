8c0767a5f84d38caf29a55010197b6dad368585f
chhavisharma-gammaedge <chhavi.shaarma@gammaedge.io>
2026-02-06 16:14:04 +0530

    ADded Test cases

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-def get_connection():
+def connection():
     if os.getenv("TESTING") == "true":
         raise Exception("DB disabled during tests")
 
