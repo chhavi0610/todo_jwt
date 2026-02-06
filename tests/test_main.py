9c10918e709d6d69cbd2c2ae857d5c5fc5ff60e0
chhavisharma-gammaedge <chhavi.shaarma@gammaedge.io>
2026-02-06 16:17:10 +0530

    ADded Test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,19 @@
+import sys
 import os
-os.environ["TESTING"] = "true"
-
 from fastapi.testclient import TestClient
 from main import app
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ["TESTING"] = "true"
+
 
 client = TestClient(app)
 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+
 
 
 
